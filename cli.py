b51453539c31735b2c90302fd9eb4942c83d2a5d
Med Mahmoud <med.mahmoud@dominodatalab.com>
2023-06-10 00:11:19 -0400

    WIP

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 import importlib
 import json
 import os
+from pprint import pprint
 import shutil
 import sys
 import textwrap
@@ -10,18 +11,20 @@ import click
 from util import paths_relative_to
 from util import paths_within
 
+import procurement
+
 
 relpath = paths_relative_to(__file__)
 
 
-context_file = relpath("context.json")
+CONTEXT_FILE = relpath("context.json")
 
-repo_dir = relpath("repos")
+REPO_DIR = relpath("repos")
 
-if not os.path.exists(repo_dir):
-    os.makedirs(repo_dir)
+if not os.path.exists(REPO_DIR):
+    os.makedirs(REPO_DIR)
 
-in_repo_dir = paths_within(repo_dir)
+in_repo_dir = paths_within(REPO_DIR)
 
 
 def registry_from_context(context_name):
@@ -36,14 +39,13 @@ def registry_from_context(context_name):
         )
 
 
-
 @click.group()
 def cli():
     """Traverse complicated procurement trees."""
 
 
 @cli.command()
-@click.option("-n", "--context_name", prompt=True)
+@click.option("-n", "--context-name", prompt=True)
 def init(context_name):
     """Initialize a new procurement context."""
     context_dir = in_repo_dir(context_name)
@@ -60,6 +62,7 @@ def init(context_name):
         f.write("")
 
     init_py = textwrap.dedent("""
+    from procurement import Procurement
     from procurement import Buy
     from procurement import Cost
     from procurement import Costs
@@ -70,6 +73,7 @@ def init(context_name):
     from procurement import Ingredients
     from procurement import Registry
 
+    from util import all_subclasses
     from util import procure_alias
     from util import paths_relative_to
 
@@ -80,7 +84,9 @@ def init(context_name):
     registry = Registry(
         kind=Ingredients,
         default_procurement=Craft,
-        procurements={},
+        procurements={
+            c.procurement_name(): c for c in all_subclasses(Procurement)
+        },
     )
 
 
@@ -91,17 +97,28 @@ def init(context_name):
     with open(in_context_dir("register.py"), "w") as f:
         f.writelines(init_py)
 
+    click.echo(f"Created context {context_name}")
+
+    if os.path.exists(in_repo_dir(context_name)):
+        with open(CONTEXT_FILE, "w") as f:
+            json.dump({"context": context_name}, f)
+    else:
+        raise RuntimeError(
+            f"Provided procurement context {context_name} does not exist!  "
+            f"Use `procure list` to see available contexts."
+        )
+
 
 @cli.command()
 def where():
     """Emit the location of the procurement contexts."""
-    click.echo(repo_dir)
+    click.echo(REPO_DIR)
 
 
 @cli.command()
 def list():
     """List known procurement contexts."""
-    for line in os.listdir(repo_dir):
+    for line in os.listdir(REPO_DIR):
         click.echo(line)
 
 
@@ -118,6 +135,8 @@ def remove(context_name):
         f"cannot be undone!"
     ):
         shutil.rmtree(in_repo_dir(context_name))
+        if os.path.exists(CONTEXT_FILE):
+            os.unlink(CONTEXT_FILE)
     else:
         click.echo("Removal not confirmed, aborting.")
 
@@ -127,7 +146,7 @@ def remove(context_name):
 def use(context_name):
     """Activate a known procurement context."""
     if os.path.exists(in_repo_dir(context_name)):
-        with open(context_file, "w") as f:
+        with open(CONTEXT_FILE, "w") as f:
             json.dump({"context": context_name}, f)
     else:
         raise RuntimeError(
@@ -136,17 +155,27 @@ def use(context_name):
         )
 
 
+def current_context(fail=True):
+    if os.path.exists(CONTEXT_FILE):
+        with open(CONTEXT_FILE, "r") as f:
+            data = json.load(f)
+        context = data.get("context")
+    else:
+        context = None
+
+    if fail and context is None:
+        raise LookupError("No current context")
+    else:
+        return context
+
+
 @cli.command()
 def current():
     """Emit the current procurement context."""
-    data = None
+    context = current_context()
 
-    if os.path.exists(context_file):
-        with open(context_file, "r") as f:
-            data = json.load(f)
-
-    if data:
-        click.echo(data.get("context"))
+    if context:
+        click.echo(context)
     else:
         click.echo("No current context.  Enter one with `procure use`")
 
@@ -154,25 +183,101 @@ def current():
 @cli.command()
 def quit():
     """Exit the procurement context."""
-    data = None
-
-    if os.path.exists(context_file):
-        with open(context_file, "r") as f:
-            data = json.load(f)
-
-    if data:
-        found = data.get("context")
-    else:
-        found = None
-
     try:
-        os.unlink(context_file)
+        os.unlink(CONTEXT_FILE)
         if found:
             click.echo(f"Quit context {found}")
         else:
             click.echo("No context to quit")
     except FileNotFoundError:
         click.echo("No context to quit")
+
+
+@cli.command()
+@click.option("-n", "--context-name", default=None)
+@click.option("--recipe", "--recipes", is_flag=True, default=False)
+@click.option("--registry", "--register", is_flag=True, default=False)
+def edit(context_name=None, recipe=False, registry=False):
+    """Edit the recipes for the context."""
+    found = context_name or current_context()
+
+    if found:
+        context_dir = in_repo_dir(found)
+        in_context_dir = paths_within(context_dir)
+
+        if recipe:
+            which_file = "recipes.txt"
+        elif registry:
+            which_file = "register.py"
+        else:
+            which_file = "recipes.txt"
+
+        with open(in_context_dir(which_file)) as f:
+            contents = f.read()
+        edited = click.edit(contents)
+        if edited is not None and edited != contents:
+            with open(in_context_dir(which_file), "w") as f:
+                f.write(edited)
+        else:
+            click.echo("No changes")
+    else:
+        click.echo("No context to edit")
+
+
+@cli.command()
+@click.option("-n", "--context-name", default=None)
+@click.argument("recipe_string")
+def optimize(context_name, recipe_string):
+    """Emit the depenency tree with details."""
+    found = context_name or current_context()
+    registry = registry_from_context(found)
+    result = procurement.optimize(
+        registry,
+        registry.kind.parse(recipe_string),
+    )
+    pprint(result)
+
+
+@cli.command()
+@click.option("-n", "--context-name", default=None)
+@click.argument("recipe_string")
+def dnf(context_name, recipe_string):
+    """
+    Emit the disjunctive normal form of the tree.
+
+    Use this with caution, this output can be very large.
+    """
+    found = context_name or current_context()
+    registry = registry_from_context(found)
+    tree = procurement.optimize(
+        registry,
+        registry.kind.parse(recipe_string),
+    )
+    result = procurement.dnf(tree)
+    for entry in result:
+        pprint(procurement.join_opt_results(entry))
+
+
+@cli.command()
+@click.option("-n", "--context-name", default=None)
+@click.argument("recipe_string")
+def summary(context_name, recipe_string):
+    """Emit a summary of the dependency tree without details."""
+    found = context_name or current_context()
+    registry = registry_from_context(found)
+    tree = procurement.optimize(registry, registry.kind.parse(recipe_string))
+    result = procurement.process_tree_overview(tree, pretty=True)
+    print(result)
+
+
+@cli.command()
+@click.option("-n", "--context-name", default=None)
+def recipes(context_name):
+    """Emit the products with known recipes."""
+    found = context_name or current_context()
+    registry = registry_from_context(found)
+    for (k, v) in registry.registry.items():
+        print(f"{k} ({len(v)} known)")
 
 
 if __name__ == "__main__":
