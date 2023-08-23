5d802a4eeabbd9884dc34751a066386b14562e82
Med Mahmoud <medthehatta@gmail.com>
2023-08-23 07:26:02 -0400

    Initial

---
+++ formal_vector.py
@@ -0,0 +1,239 @@
+import math
+import re
+from operator import add
+from operator import mul
+
+
+class FormalVector:
+    _ZERO = "FormalVector.zero()"
+    _registry = {}
+    _norm_lookup = {}
+
+    @classmethod
+    def named(cls, name, content=None):
+        if name not in cls._registry:
+            if content is None:
+                content = name
+            cls._registry[name] = cls(
+                components={name: 1},
+                basis={name: content},
+                name=name,
+            )
+            cls._norm_lookup[cls._norm_name(name)] = name
+        return cls._registry[name]
+
+    @classmethod
+    def _norm_name(cls, name):
+        return name.lower().replace("'", "")
+
+    @classmethod
+    def lookup(cls, name):
+        if name in cls._registry:
+            return cls._registry[name]
+        lowname = name.lower()
+        if lowname in cls._norm_lookup:
+            return cls._registry[cls._norm_lookup[lowname]]
+        matches = [
+            cls._norm_lookup[x] for x in cls._norm_lookup if lowname in x
+        ]
+        if len(matches) == 0:
+            raise LookupError(f"Could not find entry for {name}")
+        elif len(matches) == 1:
+            return cls._registry[matches[0]]
+        else:
+            raise LookupError(f"Ambiguous matches for {name}: {matches}")
+
+    @classmethod
+    def zero(cls):
+        name = cls._ZERO
+        if name not in cls._registry:
+            cls._registry[name] = cls(
+                components={},
+                basis={},
+                name=name,
+            )
+        return cls._registry[name]
+
+    @classmethod
+    def sum(cls, vectors):
+        return sum(vectors, start=cls.zero())
+
+    @classmethod
+    def componentwise(cls, func, v, w):
+        components = {}
+        basis = {}
+        keys = set(
+            list(v.components.keys()) +
+            list(w.components.keys())
+        )
+        for k in keys:
+            components[k] = func(
+                v.components.get(k, 0),
+                w.components.get(k, 0)
+            )
+            basis[k] = v.basis.get(k) or w.basis.get(k)
+        return cls(components=components, basis=basis)
+
+    @classmethod
+    def dot(cls, v, w):
+        x = cls.componentwise(lambda a, b: a * b, v, w)
+        return sum(x.components.values())
+
+    @classmethod
+    def parse_from_list(cls, lst, populate=None, fuzzy=False):
+        return cls.sum(
+            cls.parse(x, populate=populate, fuzzy=fuzzy) for x in lst
+        )
+
+    @classmethod
+    def parse(cls, s, populate=None, fuzzy=False):
+        populate = populate or (lambda x: None)
+
+        if fuzzy:
+
+            def new(x):
+                try:
+                    return cls.lookup(x)
+                except LookupError:
+                    return cls.named(x, content=populate(x))
+
+        else:
+
+            def new(x):
+                return cls.named(x, content=populate(x))
+
+        components = [
+            re.search(r"(-?\d+(?:\.\d+)?)?\s*[*]?\s*(.*)", y.strip()).groups()
+            for y in re.split(r"\s*[+]\s*", s)
+        ]
+        if len(components) == 1:
+            x = components[0]
+            return (
+                float(x[0])*new(x[1]) if x[0] else
+                new(x[1])
+            )
+        else:
+            return cls.sum(
+                (
+                    float(x[0])*new(x[1]) if x[0] else
+                    new(x[1])
+                )
+                for x in components
+            )
+
+    def __init__(self, components=None, basis=None, name=None):
+        self.name = name
+        self.components = components
+        self.basis = basis
+        if self.components.keys() != self.basis.keys():
+            raise ValueError(
+                f"Component keys and basis keys do not match! "
+                f"components={components.keys()}, basis={basis.keys()}"
+            )
+
+    def is_leaf(self):
+        return self.components == {self.name: 1}
+
+    @property
+    def nonzero_components(self):
+        return {k: v for (k, v) in self.components.items() if v != 0}
+
+    def pure(self):
+        n = len(self.nonzero_components)
+        if n == 1:
+            return self.triples()[0]
+        else:
+            raise ValueError(f"Not a pure vector (has {n} (!=1) components).")
+
+    @property
+    def content(self):
+        if self.is_leaf():
+            return self.basis[self.name]
+        else:
+            return self
+
+    def pairs(self):
+        return [(self.components[k], self.basis[k]) for k in self.components]
+
+    def triples(self):
+        return [
+            (k, self.components[k], self.basis[k]) for k in self.components
+        ]
+
+    def unit(self, k):
+        return FormalVector.named(k, self.basis[k])
+
+    def project(self, k):
+        return self.components[k] * self.unit(k)
+
+    def magnitude(self):
+        return math.sqrt(self.normsquare())
+
+    def normsquare(self):
+        return self.dot(self, self)
+
+    def normalized(self):
+        return (1/self.magnitude()) * self
+
+    def reduce(self, func, agg=add, prod=mul):
+        triples = iter(self.triples())
+        (first_name, first_count, first_base) = next(triples)
+
+        total = prod(first_count, func(first_base))
+        for (name, count, base) in triples:
+            total = agg(total, prod(count, func(base)))
+
+        return total
+
+    def __getitem__(self, item):
+        return self.components[item]
+
+    def __add__(self, other):
+        components = {}
+        basis = {}
+        keys = set(
+            list(self.components.keys()) +
+            list(other.components.keys())
+        )
+        for k in keys:
+            components[k] = (
+                self.components.get(k, 0) +
+                other.components.get(k, 0)
+            )
+            basis[k] = self.basis.get(k) or other.basis.get(k)
+        cls = type(self)
+        return cls(components=components, basis=basis)
+
+    def __rmul__(self, alpha):
+        cls = type(self)
+        return cls(
+            components={k: alpha*v for (k, v) in self.components.items()},
+            basis=self.basis,
+        )
+
+    def __sub__(self, other):
+        return self + (-1) * other
+
+    def __neg__(self):
+        return (-1) * self
+
+    def __bool__(self):
+        return self.nonzero_components != {}
+
+    def __repr__(self):
+        if self.name:
+            return self.name
+        elif self.components == {}:
+            return self._ZERO
+        elif all(v == 0 for v in self.components.values()):
+            return self._ZERO
+        else:
+            return " + ".join(
+                f"{k}" if v == 1 else
+                f"-{k}" if v == -1 else
+                f"{v} {k}"
+                for (k, v) in self.components.items()
+                if v != 0
+            )
+
+
---
+++ procurement.py
@@ -0,0 +1,314 @@
+import re
+
+from formal_vector import FormalVector
+
+
+class Ingredients(FormalVector):
+    _ZERO = "Ingredients.NONE"
+
+
+class Costs(FormalVector):
+    _ZERO = "Costs.FREE"
+
+
+class Cost:
+    cash = Costs.named("cash")
+    seconds = Costs.named("seconds")
+
+
+class Registry:
+
+    def __init__(
+        self,
+        kind,
+        procurements,
+        default_procurement,
+    ):
+        self.kind = kind
+        self.procurements = procurements
+        self.default_procurement = default_procurement
+        self.registry = {}
+
+    def lookup(self, item):
+        if isinstance(item, self.kind):
+            (name, _, _) = item.pure()
+            return self.registry[name]
+        elif isinstance(item, str):
+            return self.lookup(self.kind.lookup(item))
+        else:
+            raise LookupError(f"Cannot look up {item}")
+
+    def from_lines(self, lines, populate=None, fuzzy=False):
+        kind = self.kind
+        procurements = self.procurements
+        default_procurement = self.default_procurement
+
+        IDLE = "IDLE"
+        LINE_1 = "LINE_1"
+        LINE_2 = "LINE_2"
+
+        mode = IDLE
+
+        for line in lines:
+
+            line_nonempty = line.strip()
+            line_empty = not line_nonempty
+
+            # Start an entry
+            if mode is IDLE and line_nonempty:
+                mode = LINE_1
+                # 5 ingredient + 2 other ingredient | attribute1=foo bar | attribute2=3
+                # 5 ingredient + 2 other ingredient | attribute1=foo bar attribute2=3
+                # 5 ingredient + 2 other ingredient
+                segments = re.split(r'\s*\|\s*', line_nonempty)
+
+                # Only the first segment mark is important.  Others are for
+                # legibility.  We ignore the other segment marks by re-joining
+                # the subsequent tokens.
+                if len(segments) > 1:
+                    (output_raw, attributes_raw) = (segments[0], " ".join(segments[1:]))
+                else:
+                    (output_raw, attributes_raw) = (segments[0], "")
+
+                # Parse the output.
+                output = self.kind.parse(output_raw, populate=populate, fuzzy=fuzzy)
+
+                # Parse the attributes.
+                # They will always be a space-free identifier followed by an
+                # equals, then arbitrary data until another attr= or end of
+                # line.
+                # foo1=some data foo2=other foo3=8
+                keys = [
+                    (m.group(1), m.span())
+                    for m in re.finditer(r'([A-Za-z_][A-Za-z_0-0]*)=', attributes_raw)
+                ]
+                end_pad = [(None, (None, None))]
+
+                attributes = {}
+                for ((k, (_, start)), (_, (end, _))) in zip(keys, keys[1:] + end_pad):
+                    attributes[k] = attributes_raw[start:end].strip()
+
+                # Determine the procurement in question
+                if "procure" in attributes:
+                    procure_name = attributes.pop("procure")
+                    procurement = self.procurements.get(
+                        procure_name,
+                        self.default_procurement,
+                    )
+                else:
+                    procurement = self.default_procurement
+
+            # Continue an entry with an inputs line
+            elif mode is LINE_1 and line_nonempty:
+                mode = LINE_2
+                inputs = self.kind.parse(line_nonempty, populate=populate, fuzzy=fuzzy)
+
+            # Index the single-line (non-crafting) procurement
+            elif line_empty and mode is LINE_1:
+                entry = procurement.create(output, **attributes)
+                for product_name in entry.product_names():
+                    self.registry[product_name] = \
+                        self.registry.get(product_name, []) + [entry]
+                procurement = None
+                attributes = None
+                output = None
+                inputs = None
+                mode = IDLE
+
+            # Index the multi-line (crafting) procurement
+            elif line_empty and mode is LINE_2:
+                entry = procurement.create(output, inputs=inputs, **attributes)
+                for product_name in entry.product_names():
+                    self.registry[product_name] = \
+                        self.registry.get(product_name, []) + [entry]
+                procurement = None
+                attributes = None
+                output = None
+                inputs = None
+                mode = IDLE
+
+        return self
+
+
+class Procurement:
+
+    @property
+    def product_names(self):
+        raise NotImplementedError()
+
+    def requirements(self, demand):
+        raise NotImplementedError()
+
+    def cannot_meet(self, demand):
+        return ValueError(f"Process {self} unable to meet demand {demand}")
+
+    def _requires(
+        self,
+        cost=None,
+        raw=None,
+        ingredients=None,
+        excess=None,
+    ):
+        cost = cost or Costs.zero()
+        raw = raw or Ingredients.zero()
+        ingredients = ingredients or Ingredients.zero()
+        excess = excess or Ingredients.zero()
+        return {
+            "cost": cost,
+            "raw": raw,
+            "ingredients": ingredients,
+            "excess": excess,
+        }
+
+
+class Craft(Procurement):
+
+    @classmethod
+    def create(cls, output, inputs, seconds=None, **kwargs):
+        if len(output.nonzero_components) > 1:
+            return CraftHeterogeneous(output, inputs, seconds=seconds, **kwargs)
+        else:
+            return CraftHomogeneous(output, inputs, seconds=seconds, **kwargs)
+
+    def __init__(self, output, inputs, seconds=None):
+        self.output = output
+        self.inputs = inputs
+        self.seconds = None if seconds is None else float(seconds)
+
+
+class CraftHomogeneous(Craft):
+
+    def product_names(self):
+        (name, _, _) = self.output.pure()
+        return [name]
+
+    def requirements(self, demand=None):
+        if demand is None:
+            demand = self.output
+
+        if demand == demand.zero():
+            return self._requires()
+
+        (name, count, _) = self.output.pure()
+
+        demand_components = demand.nonzero_components
+        if (
+            len(demand_components) != 1 or
+            list(demand_components) != [name]
+        ):
+            raise self.cannot_meet(demand)
+
+        (_, amount, _) = demand.pure()
+
+        if self.seconds is not None:
+            rate = count / self.seconds
+            return self._requires(
+                ingredients=amount/rate * self.inputs,
+                cost=self.seconds * Cost.seconds,
+            )
+
+        else:
+            return self._requires(
+                ingredients=amount/count * self.inputs,
+            )
+
+
+class CraftHeterogeneous(Craft):
+
+    def product_names(self):
+        return list(self.output.nonzero_components.keys())
+
+    def requirements(self, demand=None):
+        if demand is None:
+            demand = self.output
+
+        if demand == demand.zero():
+            return self._requires()
+
+        demand_components = demand.nonzero_components
+        output_components = self.output.nonzero_components
+
+        if not set(demand_components).issubset(output_components):
+            raise self.cannot_meet(demand)
+
+        ratios = {
+            k: demand_components[k] / output_components[k]
+            for k in demand_components
+        }
+
+        # We return the minimum requirements to meet the demand.  Note that
+        # this can produce excess of the output products.
+
+        bottleneck = max(ratios.values())
+
+        if self.seconds is not None:
+            reqs = bottleneck * self.seconds * self.inputs
+            return self._requires(
+                ingredients=reqs,
+                excess=bottleneck*self.output - demand,
+                cost=self.seconds * Cost.seconds,
+            )
+
+        else:
+            return self._requires(
+                ingredients=bottleneck * self.inputs,
+                excess=bottleneck*self.output - demand,
+            )
+
+
+class Buy(Procurement):
+
+    @classmethod
+    def create(cls, product, cash, **kwargs):
+        return cls(product, cash, **kwargs)
+
+    def __init__(self, product, cash):
+        self.product = product
+        self.cash = float(cash)
+
+    def product_names(self):
+        (name, _, _) = self.product.pure()
+        return [name]
+
+    def requirements(self, demand=None):
+        if demand is None:
+            demand = self.product
+
+        if demand == demand.zero():
+            return self._requires()
+
+        (_, count, _) = self.product.pure()
+        (_, amount, _) = demand.pure()
+
+        return self._requires(
+            cost=amount/count * self.cash * Cost.cash,
+        )
+
+
+class Gather(Procurement):
+
+    @classmethod
+    def create(cls, product, seconds, **kwargs):
+        return cls(product, seconds, **kwargs)
+
+    def __init__(self, product, seconds):
+        self.product = product
+        self.seconds = float(seconds)
+
+    def product_names(self):
+        (name, _, _) = self.product.pure()
+        return [name]
+
+    def requirements(self, demand=None):
+        if demand is None:
+            demand = self.product
+
+        if demand == demand.zero():
+            return self._requires()
+
+        (_, count, _) = self.product.pure()
+        (_, amount, _) = demand.pure()
+
+        return _requires(
+            cost=amount/count * self.seconds * Cost.seconds,
+        )
